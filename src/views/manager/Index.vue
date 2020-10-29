6d46f4532126b6e7a1c74f405e27f945650d9da1
YuDGang <liang1304568590@163.com>
2020-10-30 01:05:46 +0800

    图床位置转移

--- src/views/manager/Index.vue
+++ src/views/manager/Index.vue
@@ -4,7 +4,7 @@
     <!-- 用于接受管理页面，租赁页面，订单页，我的页面 -->
     <router-view></router-view>
     <van-tabbar route active-color="#1659a0">
-      <van-tabbar-item icon="https://cdn4.iconfinder.com/data/icons/shopping-e-commerce-1/128/Trust-Handshake-Partner-Deal-64.png" to="/manager/home">租赁</van-tabbar-item>
+      <van-tabbar-item icon="https:///cdn4.iconfinder.com/data/icons/shopping-e-commerce-1/128/Trust-Handshake-Partner-Deal-64.png" to="/manager/home">租赁</van-tabbar-item>
       <van-tabbar-item icon="https://cdn4.iconfinder.com/data/icons/shopping-e-commerce-1/128/Special-Top-List-64.png" to="/manager/rent">出租</van-tabbar-item>
       <van-tabbar-item icon="https://cdn4.iconfinder.com/data/icons/shopping-e-commerce-1/128/Tag-Price-Label-64.png" to="/manager/user">我的</van-tabbar-item>
     </van-tabbar>
